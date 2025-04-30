9816e279bd25d94016c379db6b9bcd4e210f1c29
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-04-30 17:27:40 +0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,7 @@ export default function RootLayout({
           ${robotoCondensed.variable}
           ${unbounded.variable}
           antialiased
+          w-full
         `}
       >
         <ClientLayout>
