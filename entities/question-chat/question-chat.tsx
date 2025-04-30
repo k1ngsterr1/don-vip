21c62d11514f0b746fcfa2dec732eb91c9ca17d8
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2025-04-30 16:21:07 +0500

    fix: fix question

--- entities/question-chat/question-chat.tsx
+++ entities/question-chat/question-chat.tsx
@@ -11,7 +11,7 @@ export const QuestionChat: React.FC<IQuestionChat> = ({ isQuestionChat }) => {
   return (
     <>
       {isQuestionChat && (
-        <button className="bg-blue fixed bottom-[90px] right-[16px] rounded-full w-[64px] h-[64px] text-white flex items-center justify-center">
+        <button className="bg-blue fixed bottom-[90px] md:bottom-[16px] right-[16px] rounded-full w-[64px] h-[64px] text-white flex items-center justify-center">
           <MessageCircleQuestionIcon
             className="text-white"
             width={26}
