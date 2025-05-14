5d8598392c10500caba44fa31a52fbe34f6c0370
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-14 12:21:12 +0500

    fix: fix yandex error

--- widgets/ui/order-page/order-summary/order-summary.tsx
+++ widgets/ui/order-page/order-summary/order-summary.tsx
@@ -101,7 +101,7 @@ export function OrderSummary({
           {isLoading ? (
             <div className="flex items-center justify-center">
               <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-white mr-2"></div>
-              Loading...
+              {t("summary.redirecting")}{" "}
             </div>
           ) : (
             t("summary.buyNow")
