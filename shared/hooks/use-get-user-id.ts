f9e4b568c723e56d3d7bd48ed0d8f0409b249ce3
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-18 20:23:17 +0500

    fix: fix use get user id

--- shared/hooks/use-get-user-id.ts
+++ shared/hooks/use-get-user-id.ts
@@ -2,48 +2,40 @@ import { useAuthStore } from "@/entities/auth/store/auth.store";
 import { apiClient } from "../config/apiClient";
 
 export const getUserId = async (): Promise<string> => {
-  if (typeof window === "undefined") {
+  if (typeof window === "undefined")
     throw new Error("❌ getUserId called on server");
-  }
 
   const { user, setUser } = useAuthStore.getState();
-
   const isNumber = (val: string) => /^\d+$/.test(val);
 
-  if (user?.id && isNumber(user.id.toString())) {
-    return user.id.toString();
-  }
-
+  if (user?.id && isNumber(user.id.toString())) return user.id.toString();
 
   try {
-    const currentUser = await apiClient.get("/user/me");
-    const fetchedUser = currentUser.data;
-
+    const response = await apiClient.get("/user/me");
+    const fetchedUser = response.data;
 
     if (fetchedUser?.id && isNumber(fetchedUser.id.toString())) {
       setUser(fetchedUser);
       return fetchedUser.id.toString();
-    } else {
-      console.warn(
-        "⚠️ [Auth] Получен некорректный user.id от /user/me:",
-        fetchedUser?.id
-      );
     }
-  } catch (err) {
+  } catch (error: any) {
+    if (error.response?.status === 401) {
+      console.warn("🔒 [Auth] 401 Unauthorized, создаём гостя...");
+    } else {
+      console.warn("⚠️ [Auth] Ошибка при /user/me:", error);
+    }
   }
 
   try {
     const res = await apiClient.post("/auth/guest");
     const guestUser = res.data;
 
-
     if (guestUser?.id && isNumber(guestUser.id.toString())) {
       setUser(guestUser);
       return guestUser.id.toString();
-    } else {
-      throw new Error("Некорректный ID гостя");
     }
+    throw new Error("Некорректный ID гостя");
   } catch (error) {
-    throw new Error("Не удалось создать гостевого пользователя");
+    throw new Error("❌ Не удалось создать гостевого пользователя");
   }
 };
