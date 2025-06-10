1125ccb5d4c3317d4b7af0e399cb6b1a5492b1da
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-06-10 14:13:38 +0500

    feat: add metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,30 @@ export default async function LocaleLayout({
     <html>
       <head>
         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZYRR1HX71W"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(98661639, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+              });
+            `,
+          }}
+        />
+        <Script
           id="jivo-widget"
           src="//code.jivo.ru/widget/LzgQISOnC6"
           strategy="afterInteractive"
